6dfb82b4d0baa2fff81270d0aec9af711496a3ef
Chosan <179817004@qq.com>
2018-06-12 03:03:10 +0800

    推服务器

--- server/proxy.js
+++ server/proxy.js
@@ -64,7 +64,10 @@ Proxy.prototype.execute = async function (targetURL, data, proxyUrl, method = 'P
         url: targetURL,
         timeout: timeout,
         encoding: null,
-        form: data
+        form: data,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36"
+        }
     };
 
     let proxyurl = proxyUrl || proxyList.shift();
